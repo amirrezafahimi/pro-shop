e5020a04be37026b74f51204a3439cfbc06828e0
Amirreza Fahimidero <amirreza.fahimidero@snapp.cab>
2021-08-06 13:30:33 +0430

    added product specific detail page.

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+def get_routes(request):
+    routes = [
+        "/api/products",
+        "/api/products/create/",
+
+        "/api/products/upload",
+
+        "/api/products/<id>/reviews/",
+
+        "/api/products/top/",
+        "/api/products/<id>/",
+
+        "/api/products/delete/<id>/",
+        "/api/products/<upload>/<id>/"
+    ]
+    return JsonResponse(routes, safe=False)
