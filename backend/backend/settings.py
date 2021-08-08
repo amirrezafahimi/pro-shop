90fee21db25e64ba3c8c57669c082f8eed7e8937
Amirreza Fahimidero <amirreza.fahimidero@snapp.cab>
2021-08-08 16:09:27 +0430

    added user authentication for back-end side

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,14 +43,14 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication"
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
 }
 
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,30 +1,47 @@
-from django.http import JsonResponse
-# from .products import products
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from .models import Product
-from .serializer import ProductSerializer
+from django.contrib.auth.models import User
+from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+
+        for key, value in serializer.items():
+            data[key] = value
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 # Create your views here.
 @api_view(["GET"])
-def get_routes(request):
-    routes = [
-        "/api/products.py",
-        "/api/products.py/create/",
+@permission_classes([IsAuthenticated])
+def get_user_profile(request):
+    user = request.user
 
-        "/api/products.py/upload",
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
-        "/api/products.py/<id>/reviews/",
 
-        "/api/products.py/top/",
-        "/api/products.py/<id>/",
-
-        "/api/products.py/delete/<id>/",
-        "/api/products.py/<upload>/<id>/"
-    ]
-    return Response(routes)
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
