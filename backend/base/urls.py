e69f6984856b1efc13ca67a24fe4144a2b7f517a
Amirreza Fahimidero <amirreza.fahimidero@snapp.cab>
2021-08-08 09:45:33 +0430

    adding token authentication.

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path("", views.get_routes, name="routes"),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("products/", views.get_products, name="products"),
     path("products/<str:pk>", views.get_product, name="product")
 ]
