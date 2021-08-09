3c5991dc8cda9d573f2ab51f524f5cd3fc07f395
Amirreza Fahimidero <amirreza.fahimidero@snapp.cab>
2021-08-09 15:16:50 +0430

    user register added.

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -4,6 +4,7 @@ from ..views import user_views
 urlpatterns = [
     path('login/', user_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("profile/", user_views.get_user_profile, name="users-profile"),
+    path("profile/update/", user_views.update_user_profile, name="user-profile-update"),
     path("register/", user_views.register_user, name="register"),
     path("", user_views.get_users, name="users")
 ]
