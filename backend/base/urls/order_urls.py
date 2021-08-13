d42979e5e8881b3260402da0e2f493cc220a9bee
Amirreza Fahimidero <amirreza.fahimidero@snapp.cab>
2021-08-13 16:34:16 +0430

    creating checkout process.

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -3,4 +3,5 @@ from ..views import order_views
 
 urlpatterns = [
     path("add/", order_views.add_order_items, name="orders-add"),
+    path("<str:pk>/", order_views.get_order_by_id, name="user-order"),
 ]
