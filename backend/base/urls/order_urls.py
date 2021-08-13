3828b758cf0e0139ae9136a5d6fef64966d3fcf7
Amirreza Fahimidero <amirreza.fahimidero@snapp.cab>
2021-08-13 19:54:33 +0430

    added order screen

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -4,4 +4,5 @@ from ..views import order_views
 urlpatterns = [
     path("add/", order_views.add_order_items, name="orders-add"),
     path("<str:pk>/", order_views.get_order_by_id, name="user-order"),
+    path("<str:pk>/pay/", order_views.update_order_to_paid, name="pay"),
 ]
--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 from rest_framework.decorators import api_view, permission_classes
@@ -60,7 +62,7 @@ def add_order_items(request):
         return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_order_by_id(request, pk):
     user = request.user
@@ -75,3 +77,14 @@ def get_order_by_id(request, pk):
                      status=status.HTTP_400_BAD_REQUEST)
     except:
         return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def update_order_to_paid(request, pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+    return Response("Order was paid")
