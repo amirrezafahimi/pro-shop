7c7887bae440c012305cd3a5264009b558e04174
Amirreza Fahimidero <amirreza.fahimidero@snapp.cab>
2021-08-13 22:54:56 +0430

    payment completed.

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -3,6 +3,8 @@ from ..views import order_views
 
 urlpatterns = [
     path("add/", order_views.add_order_items, name="orders-add"),
+    path("myorders/", order_views.get_my_orders, name="myorders"),
+
     path("<str:pk>/", order_views.get_order_by_id, name="user-order"),
     path("<str:pk>/pay/", order_views.update_order_to_paid, name="pay"),
 ]
--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -79,6 +79,16 @@ def get_order_by_id(request, pk):
         return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_my_orders(request):
+    user = request.user
+    orders = user.order_set.all()
+    serializer = OrderSerializer(orders, many=True)
+
+    return Response(serializer.data)
+
+
 @api_view(["PUT"])
 @permission_classes([IsAuthenticated])
 def update_order_to_paid(request, pk):
