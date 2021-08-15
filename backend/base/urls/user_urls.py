0a3163aa95684b785791ea7761cdd2f97860fe94
Amirreza Fahimidero <amirreza.fahimidero@snapp.cab>
2021-08-15 13:48:12 +0430

    update user profile added for admin user.

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("profile/", user_views.get_user_profile, name="users-profile"),
     path("profile/update/", user_views.update_user_profile, name="user-profile-update"),
     path("register/", user_views.register_user, name="register"),
-    path("", user_views.get_users, name="users")
+    path("", user_views.get_users, name="users"),
+    path("delete/<str:pk>/", user_views.delete_user, name="user-delete"),
+    path("<str:pk>/", user_views.get_user_by_id, name="user"),
+    path("update/<str:pk>/", user_views.update_user, name="user-update"),
 ]
