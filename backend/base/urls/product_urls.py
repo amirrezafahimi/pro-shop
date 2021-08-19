1caf9cc74f2ad16427f271e81cd69ec9f6882a4c
Amirreza Fahimidero <amirreza.fahimidero@snapp.cab>
2021-08-19 13:23:17 +0430

    upload image done.

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", product_views.get_products, name="products"),
 
     path("create/", product_views.create_product, name="product-create"),
+    path("upload/", product_views.upload_image, name="image-upload"),
     path("<str:pk>/", product_views.get_product, name="product"),
 
     path("delete/<str:pk>/", product_views.delete_product, name="product-delete"),
